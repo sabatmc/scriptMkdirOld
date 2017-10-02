1a2e3fd958c707703c7f2a36d82ebc9fdafb3cad
sabatmc <32088873+sabatmc@users.noreply.github.com>
2017-10-02 20:38:56 +0300

    Create excScript.sh

---
+++ excScript.sh
@@ -0,0 +1,14 @@
+# excScript
+# replace 
+n=1
+for N in {0.1,1,5}
+do
+mkdir Name${n}
+cd Name${n}
+cp ../inputOrig.txt ./
+cp ../input.txt ./
+sed -e "s/xxxx/${N}/" inputOrig.txt > input.txt
+#here you can call a executable that will run your simulation using the input parameter
+cd ../
+n=$(( $n + 1 ))
+done
