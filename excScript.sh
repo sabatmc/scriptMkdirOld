2e93a2a8e44e2b9ce4e7dd99cb8ae2ee9eff396c
Macole <macole@ubuntu.ubuntu-domain>
2017-10-03 07:18:18 -0400

    modified script

--- excScript.sh
+++ excScript.sh
@@ -3,8 +3,8 @@
 n=1
 for N in {0.1,1,5}
 do
-mkdir Name${n}
-cd Name${n}
+mkdir Macole${n}
+cd Macole${n}
 cp ../inputOrig.txt ./
 cp ../input.txt ./
 sed -e "s/xxxx/${N}/" inputOrig.txt > input.txt
