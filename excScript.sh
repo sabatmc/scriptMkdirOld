08ba94cd5adef939e944102b731046074723b714
Ihssan Matar <ihssantarekmatar@gmail.com>
2017-10-05 13:02:46 +0300

    Modified the excScript to include Ihssan's name

--- excScript.sh
+++ excScript.sh
@@ -5,8 +5,8 @@ n=1
 for N in 0.1 1 50 
 do
 echo "Creating Macole$n folder with N=$N"
-mkdir Macole${n}
-cd Macole${n}
+mkdir Ihssan${n}
+cd Ihssan${n}
 cp ../inputOrig.txt ./
 cp ../input.txt ./
 sed -e "s/xxxx/${N}/" inputOrig.txt > input.txt
