5b3aecacb2945c49f2160133fae8027ccecbc947
Macole Sabat <macole.sabat@fty.balamand.edu.lb>
2018-10-22 05:28:54 -0400

    Back to original version

--- excScript.sh
+++ excScript.sh
@@ -4,14 +4,14 @@
 n=1
 for N in 0.1 1 50 
 do
-echo "Creating george$n folder with N=$N"
+echo "Creating Macole$n folder with N=$N"
 
-mkdir george${n}
-cd george${n}
+mkdir Macole${n}
+cd Macole${n}
 cp ../inputOrig.txt ./
 cp ../input.txt ./
 sed -e "s/xxxx/${N}/" inputOrig.txt > input.txt
-#here you can call a executable that will run your simulation using the input parameter
+#here you can call an executable that will run your simulation using the input parameter
 cd ../
 n=$(( $n + 1 ))
 done
