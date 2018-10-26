0bfb310d148a119e565d3912d23b470a657bc95f
user <keven@ubuntu.ubuntu-domain>
2018-10-26 15:46:09 +0300

    modified scrpit

--- excScript.sh
+++ excScript.sh
@@ -1,13 +1,13 @@
 # excScript
-# replace Macole with your name 
+# replace Keven with your name 
 
 n=1
 for N in 0.1 1 50 
 do
-echo "Creating Macole$n folder with N=$N"
+echo "Creating Keven$n folder with N=$N"
 
-mkdir Macole${n}
-cd Macole${n}
+mkdir Keven${n}
+cd Keven${n}
 cp ../inputOrig.txt ./
 cp ../input.txt ./
 sed -e "s/xxxx/${N}/" inputOrig.txt > input.txt
