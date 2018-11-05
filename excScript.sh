9e940a8685c7e6db6bbf09f4e3fa12e5abc3bb5f
hilal kawam <hilalkawam92@gmail.com>
2018-11-05 15:13:28 -0500

    Modified the excScript to include omars name

--- excScript.sh
+++ excScript.sh
@@ -3,8 +3,8 @@
 n=1
 for N in 0.1 1 5
 do
-mkdir Sarkis${n}
-cd Sarkis${n}
+mkdir hilal${n}
+cd hilal${n}
 cp ../inputOrig.txt ./
 cp ../input.txt ./
 sed -e "s/xxxx/${N}/" inputOrig.txt > input.txt
