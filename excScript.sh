3c3b67fd7a9e653aa8fdb6e909854a7199acb94a
Sharbel <sharbel.nashar@std.balamand.edu.lb>
2019-02-26 10:02:11 +0200

    modified excScript to switch Macole to my name

--- excScript.sh
+++ excScript.sh
@@ -1,13 +1,13 @@
 # excScript
-# replace Macole with your name 
+# replace Sharbel with your name 
 
 n=1
 for N in 0.1 1 50 
 do
-echo "Creating Macole$n folder with N=$N"
+echo "Creating Sharbel$n folder with N=$N"
 
-mkdir Macole${n}
-cd Macole${n}
+mkdir Sharbel${n}
+cd Sharbel${n}
 cp ../inputOrig.txt ./
 cp ../input.txt ./
 sed -e "s/xxxx/${N}/" inputOrig.txt > input.txt
