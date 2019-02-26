dc70ac760d6bd032ea7d346668d70b346e063f06
Tarek Raad <clara.haydar@std.balamand.edu.lb>
2019-02-26 10:08:13 +0200

    fro9m macole to tarek

--- excScript.sh
+++ excScript.sh
@@ -1,13 +1,13 @@
 # excScript
-# replace Macole with your name 
+# replace tarek with your name 
 
 n=1
 for N in 0.1 1 50 
 do
-echo "Creating Macole$n folder with N=$N"
+echo "Creating tarek$n folder with N=$N"
 
-mkdir Macole${n}
-cd Macole${n}
+mkdir tarek${n}
+cd tarek${n}
 cp ../inputOrig.txt ./
 cp ../input.txt ./
 sed -e "s/xxxx/${N}/" inputOrig.txt > input.txt
